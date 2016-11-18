c182ca39def1c3f391feb2a25c48e4d34e08d8a9
Natalie Claunch <nmclaunch@ufl.edu>
2016-11-18 14:25:52 -0500

    Added get GC content function

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,4 +1,17 @@
 #HouseElf Ear length Analysis for Dr. Granger
-
+library(dplyr)
+library(stringr)
 
 elf_data<- read.csv("data/houseelf_earlength_dna_data_1_two.csv", stringsAsFactors = FALSE)
+
+
+get_gc_content<-function(sequence){ 
+  #calculate percentage of g and c in a DNA sequence
+  lower_seq<-tolower(sequence)
+  Gs <- str_count(lower_seq, "g")
+  Cs <- str_count(lower_seq, "c")
+  gc_content <-(Gs + Cs) / str_length(lower_seq)*100
+  return(gc_content)
+}
+
+get_gc_content(elf_data)
