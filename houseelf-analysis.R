3aeb8b92ed951ed622567803356006e6747306bc
Natalie Claunch <nmclaunch@ufl.edu>
2016-11-18 12:14:39 -0500

    modifiede code to reflect name change to original file

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,4 +1,4 @@
 #HouseElf Ear length Analysis for Dr. Granger
 
 
-elf_data<- read.csv("data/houseelf-earlength-data.csv", stringsAsFactors = FALSE)
+elf_data<- read.csv("data/houseelf_earlength_data_1.csv", stringsAsFactors = FALSE)
